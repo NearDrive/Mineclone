8a23e9b473c886ef73a5b8133eae7c505f3ecaef
NearDrive <NearDrive@gmail.com>
2026-01-21 19:18:14 +0100

    Fix RenderTest GLAD include order

--- src/renderer/RenderTest.cpp
+++ src/renderer/RenderTest.cpp
@@ -1,7 +1,7 @@
-#include "renderer/RenderTest.h"
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include "renderer/RenderTest.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
