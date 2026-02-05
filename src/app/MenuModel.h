81289dc4a351a0a76868437516bf356c49cae128
NearDrive <NearDrive@gmail.com>
2026-02-05 22:25:24 +0100

    Enable curvature by default and add menu button UI

--- src/app/MenuModel.h
+++ src/app/MenuModel.h
@@ -14,7 +14,7 @@ enum class MenuChoice {
 struct MenuModel {
     static constexpr std::string_view kMainMenuTitle = "[MENU] 1-New 2-Load 3-Exit";
     static constexpr std::string_view kMainMenuMissingTitle = "[MENU] No save found. 1-New 2-Load 3-Exit";
-    static constexpr std::string_view kPauseMenuTitle = "[PAUSED] 1-Continue 2-Save 3-Exit to Menu";
+    static constexpr std::string_view kPauseMenuTitle = "[PAUSED] 1-Continue 2-Save 3-Load 4-Settings 5-Leave";
     static constexpr std::string_view kLoadingTitle = "[LOADING] Preparing world...";
 };
 
